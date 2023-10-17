2c6d8220f81113438d7e870a8af731f2510282b1
crab85193 <crab85193@gmail.com>
2023-10-17 17:35:42 +0900

    add: main_appを追加

--- main_app/urls.py
+++ main_app/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("", include("main_app.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
 ]
