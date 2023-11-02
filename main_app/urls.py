c4e53be1e74bb070e8dd3292f3d0ac7f6e48e567
crab85193 <crab85193@gmail.com>
2023-11-02 21:06:32 +0900

    fix: プロジェクト名を変更

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for shift_l project.
+URL configuration for main_app.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
