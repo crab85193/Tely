a7a8a8f985a29580c54f1bef508265266bc2ada3
crab85193 <crab85193@gmail.com>
2023-11-12 20:18:34 +0900

    fix: ssl化実施

--- tely/settings.py
+++ tely/settings.py
@@ -28,6 +28,7 @@ DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 # Application definition
 
