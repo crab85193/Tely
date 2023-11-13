3e5f63f9b5abe73c9db99569e5777f00a20404b7
crab85193 <crab85193@gmail.com>
2023-11-14 02:30:24 +0900

    update: typeフィールドとis_checkフィールドを追加

--- main_app/models/user_notice.py
+++ main_app/models/user_notice.py
@@ -4,9 +4,23 @@ from django.utils import timezone
 from .user import User
 
 class UserNotice(models.Model):
+    WARNING = 0
+    DANGER  = 1
+    SUCCESS = 2
+    PRIMARY = 3
+
+    NOTICETYPE = (
+        (WARNING, 'Warning'),
+        (DANGER , 'Danger' ),
+        (SUCCESS, 'Success'),
+        (PRIMARY, 'Primary'),
+    )
+
     id       = models.UUIDField(primary_key=True, editable=False, blank=False, null=False, default=uuid.uuid4)
     datetime = models.DateTimeField(blank=False, null=False, default=timezone.now)
     user     = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False, related_name="notice_user")
     title    = models.CharField(max_length=100, blank=False, null=False, default="No Title")
     message  = models.TextField(blank=False, null=False, default="No Message")
+    type     = models.IntegerField(blank=False, null=False, default=0, choices=NOTICETYPE)
     url      = models.URLField(blank=True, null=True)
+    is_check = models.BooleanField(blank=False, null=False, default=False)
