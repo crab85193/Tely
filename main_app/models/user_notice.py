efcd09b9815f7d97c6159cfa12f0f04e48136c4d
crab85193 <crab85193@gmail.com>
2023-11-13 22:05:01 +0900

    fix: 変数のスペルミスを修正

--- main_app/models/user_notice.py
+++ main_app/models/user_notice.py
@@ -6,7 +6,7 @@ from .user import User
 class UserNotice(models.Model):
     id       = models.UUIDField(primary_key=True, editable=False, blank=False, null=False, default=uuid.uuid4)
     datetime = models.DateTimeField(blank=False, null=False, default=timezone.now)
-    user     = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, null=False, related_name="user")
+    user     = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, null=False, related_name="notice_user")
     title    = models.CharField(max_length=100, blank=False, null=False, default="No Title")
     message  = models.TextField(blank=False, null=False, default="No Message")
     url      = models.URLField(blank=True, null=True)
